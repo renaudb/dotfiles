0c8542b60bc53eadae66615c4f0443f1f4a70fa8
renaudb <to.rhino@gmail.com>
2013-04-23 18:27:20 -0400

    update emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,5 +1,5 @@
 ;; -----------------------------------------------
-;; INITIALIZE STARTUP OPTIONS
+;; STARTUP OPTIONS
 ;; -----------------------------------------------
 
 ;; Set Font Size
@@ -7,10 +7,6 @@
 
 ;; No Startup Message
 (setq inhibit-startup-message t)
-
-;; -----------------------------------------------
-;; SETUP UP BUILT IN MODES
-;; -----------------------------------------------
 
 ;; Disable the menubar and toolbar
 (menu-bar-mode -1)
@@ -25,9 +21,6 @@
 ;; Show matching parenthesis
 (show-paren-mode t)
 
-;; Set C/C++ indent to 2 spaces
-(setq c-basic-offset 2)
-
 ;; Reload etags file on change
 (setq tags-revert-without-query t)
 
@@ -41,7 +34,7 @@
       )
 
 ;; -----------------------------------------------
-;; INITIALIZE USER DEFINED SHORTCUTS
+;; USER DEFINED SHORTCUTS
 ;; -----------------------------------------------
 
 ;; Compile shortcut
@@ -51,7 +44,7 @@
 (global-set-key (kbd "<C-tab>") 'buffer-menu)
 
 ;; -----------------------------------------------
-;; INITIALIZE USER DEFINED FUNCTIONS
+;; USER DEFINED FUNCTIONS
 ;; -----------------------------------------------
 
 ;; Special prints
@@ -68,51 +61,161 @@
     (ps-print-buffer-with-faces)))
 
 ;; -----------------------------------------------
-;; LOAD EMACS MODES
+;; PACKAGES
 ;; -----------------------------------------------
 
-;; Load emacs mode
+;; Load built-in emacs modes
 (require 'cl)
 (require 'uniquify)
 (require 'whitespace)
 
-;; Load el-get modes
+;; Load el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(require 'el-get)
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
 
-;; Set mode list
+;; Set local sources
 (setq el-get-sources
-      '(package
-
-	;; Language modes
-	js2-mode
-	lua-mode
-	php-mode
-	python-mode
-	ruby-mode
-	scala-mode
-	R-mode
-
-	;; Default to Google style for C/C++
-	google-c-style
-
-	;; Color theme modes
-	color-theme
-	color-theme-tango-2
+      '(
+	(:name emacs-clang-complete-async
+	       :type github
+	       :pkgname "Golevka/emacs-clang-complete-async"
+	       :description "An emacs plugin to complete C and C++ code using libclang."
+	       :build ("make")
+	       :post-init (setq ac-clang-complete-executable (expand-file-name "clang-complete"))
+	       )
 	))
-(el-get)
+
+;; Set package list
+(setq my-packages
+      (append
+       '(
+	 ;; Language modes
+	 ess
+	 js2-mode
+	 lua-mode
+	 ;matlab-mode
+	 php-mode
+	 python-mode
+	 ruby-mode
+	 scala-mode
+
+	 ;; Google style
+	 google-c-style
+
+	 ;; Autocomplete
+	 auto-complete
+	 ;auto-complete-clang
+	 auto-complete-latex
+
+	 ;; Syntax checking.
+	 flymake
+
+	 ;; Super modes
+	 jedi
+	 eclim
+	 auctex
+
+	 ;; Random modes
+	 rainbow-mode
+
+	 ;; Color themes
+	 color-theme
+	 color-theme-solarized
+	 )
+       (mapcar 'el-get-source-name el-get-sources)))
+
+;; Sync el-get packages.
+(el-get 'sync my-packages)
+
+;; -----------------------------------------------
+;; COLOR THEME
+;; -----------------------------------------------
 
 ;; Set the color theme.
 (color-theme-solarized-dark)
 
+;; -----------------------------------------------
+;; AUTO-COMPLETE
+;; -----------------------------------------------
+
+;; Load auto-complete.
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; Use auto-complete for everything.
+(global-auto-complete-mode t)
+
+;; -----------------------------------------------
+;; C/C++ MODE
+;; -----------------------------------------------
+
+;; Set indent to 2 spaces
+(setq c-basic-offset 2)
+
 ;; Set C-mode to Google.
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 
-;; Autoload octave mode on MATLAB files.
-(add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
+;; Setup C/C++ clang auto-complete.
+(require 'auto-complete-clang-async)
+(defun ac-cc-mode-setup ()
+  (setq ac-sources '(ac-source-clang-async))
+  (ac-clang-launch-completion-process)
+)
+(add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+
+;; Add standard library to clang completion.
+(setq ac-clang-flags
+      (mapcar (lambda (item)(concat "-I" item))
+              (split-string
+               "
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/../../../../include/c++/4.8.0
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/../../../../include/c++/4.8.0/x86_64-unknown-linux-gnu
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/../../../../include/c++/4.8.0/backward
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/include
+ /usr/local/include
+ /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/include-fixed
+ /usr/include
+"
+               )))
 
 ;; -----------------------------------------------
-;; INITIALIZE MODES AND OTHER OPTIONS
+;; JAVA MODE
+;; -----------------------------------------------
+
+;; Start eclim.
+(require 'eclim)
+(require 'eclimd)
+(global-eclim-mode)
+
+;; Setup Java eclim auto-complete.
+(require 'ac-emacs-eclim-source)
+(ac-emacs-eclim-config)
+
+;; -----------------------------------------------
+;; PYTHON MODE
+;; -----------------------------------------------
+
+;; Set Python-mode to use Jedi.
+(add-hook 'python-mode-hook 'jedi:setup)
+
+;; -----------------------------------------------
+;; LATEX MODE
+;; -----------------------------------------------
+
+;; Set auctex to PDF.
+(setq TeX-PDF-mode t)
+
+;; Load flyspell for text correction.
+(add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
+
+;; -----------------------------------------------
+;; WHITESPACE MODE
 ;; -----------------------------------------------
 
 ;; Highlight trailing characters
