464146361059542c7de978208bebd067d994c684
Renaud Bourassa <to.rhino@gmail.com>
2021-09-09 10:57:14 -0400

    Update dotfiles

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,240 +1,57 @@
-;; -----------------------------------------------
-;; STARTUP OPTIONS
-;; -----------------------------------------------
+;; Set name and email.
+(setq user-full-name "Renaud Bourassa")
+(setq user-mail-address "to.rhino@gmail.com")
 
-;; Set Font Size
-(set-face-attribute 'default nil :height 110)
+;; Load package.
+(load "package")
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
 
-;; No Startup Message
-(setq inhibit-startup-message t)
+;; Load use-package.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
 
-;; Disable the menubar and toolbar
-(menu-bar-mode -1)
+;; Force installation if missing.
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+;; Install packages.
+(use-package solarized-theme)
+
+;; Disable UI options.
+(setq inhibit-splash-screen t
+      initial-scratch-message nil)
 (tool-bar-mode -1)
+(menu-bar-mode -1)
 
-;; Show column number
-(column-number-mode t)
+;; Set esc-up/esc-down.
+(global-set-key (kbd "ESC <up>") 'backward-paragraph)
+(global-set-key (kbd "ESC <down>") 'forward-paragraph)
 
-;; Save session on exit
-(desktop-save-mode t)
+;; Show column number.
+(column-number-mode 1)
 
-;; Show matching parenthesis
-(show-paren-mode t)
+;; Y/n instead of Yes/no.
+(defalias 'yes-or-no-p 'y-or-n-p)
 
-;; Reload etags file on change
-(setq tags-revert-without-query t)
+;; Autoreload on file change.
+(global-auto-revert-mode t)
 
-;; Set backup file folder
-(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
-      backup-by-copying t    ; Don't delink hardlinks
-      version-control t      ; Use version numbers on backups
-      delete-old-versions t  ; Automatically delete excess backups
-      kept-new-versions 20   ; how many of the newest versions to keep
-      kept-old-versions 5    ; and how many of the old
-      )
+;: No backup files.
+(setq make-backup-files nil)
 
-;; -----------------------------------------------
-;; USER DEFINED SHORTCUTS
-;; -----------------------------------------------
+;; Default indent.
+(setq tab-width 2
+      c-basic-offset 2
+      js-indent-level 2
+      indent-tabs-mode nil)
 
-;; Compile shortcut
-(global-set-key (kbd "C-c c") 'recompile)
+;; Remove trailing whitespaces.
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; Buffer menu shortcut
-(global-set-key (kbd "<C-tab>") 'buffer-menu)
-
-;; -----------------------------------------------
-;; USER DEFINED FUNCTIONS
-;; -----------------------------------------------
-
-;; Special prints
-(defun ps-spool-buffer-no-header ()
-  "Postscript print, but without a header."
-  (interactive)
-  (let ((ps-print-header nil))
-    (ps-spool-buffer)))
-
-(defun ps-spool-buffer-with-faces-no-header ()
-  "Postscript print, but without a header."
-  (interactive)
-  (let ((ps-print-header nil))
-    (ps-print-buffer-with-faces)))
-
-;; -----------------------------------------------
-;; PACKAGES
-;; -----------------------------------------------
-
-;; Load built-in emacs modes
-(require 'cl)
-(require 'uniquify)
-(require 'whitespace)
-
-;; Load el-get
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
-
-;; Set local sources
-(setq el-get-sources
-      '(
-	(:name emacs-clang-complete-async
-	       :type github
-	       :pkgname "Golevka/emacs-clang-complete-async"
-	       :description "An emacs plugin to complete C and C++ code using libclang."
-	       :build ("make")
-	       :post-init (setq ac-clang-complete-executable (expand-file-name "clang-complete"))
-	       )
-	))
-
-;; Set package list
-(setq my-packages
-      (append
-       '(
-	 ;; Language modes
-	 ess
-	 js2-mode
-	 lua-mode
-	 ;matlab-mode
-	 php-mode
-	 python-mode
-	 ruby-mode
-	 scala-mode
-
-	 ;; Google style
-	 google-c-style
-
-	 ;; Autocomplete
-	 auto-complete
-	 ;auto-complete-clang
-	 auto-complete-latex
-
-	 ;; Syntax checking.
-	 flymake
-
-	 ;; Super modes
-	 jedi
-	 eclim
-	 auctex
-
-	 ;; Random modes
-	 rainbow-mode
-
-	 ;; Color themes
-	 color-theme
-	 color-theme-solarized
-	 )
-       (mapcar 'el-get-source-name el-get-sources)))
-
-;; Sync el-get packages.
-(el-get 'sync my-packages)
-
-;; -----------------------------------------------
-;; COLOR THEME
-;; -----------------------------------------------
-
-;; Set the color theme.
-(color-theme-solarized-dark)
-
-;; -----------------------------------------------
-;; AUTO-COMPLETE
-;; -----------------------------------------------
-
-;; Load auto-complete.
-(require 'auto-complete-config)
-(ac-config-default)
-
-;; Use auto-complete for everything.
-(global-auto-complete-mode t)
-
-;; -----------------------------------------------
-;; C/C++ MODE
-;; -----------------------------------------------
-
-;; Set indent to 2 spaces
-(setq c-basic-offset 2)
-
-;; Set C-mode to Google.
-(add-hook 'c-mode-common-hook 'google-set-c-style)
-
-;; Setup C/C++ clang auto-complete.
-(require 'auto-complete-clang-async)
-(defun ac-cc-mode-setup ()
-  (setq ac-sources '(ac-source-clang-async))
-  (ac-clang-launch-completion-process)
-)
-(add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
-
-;; Add standard library to clang completion.
-(setq ac-clang-flags
-      (mapcar (lambda (item)(concat "-I" item))
-              (split-string
-               "
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/../../../../include/c++/4.8.0
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/../../../../include/c++/4.8.0/x86_64-unknown-linux-gnu
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/../../../../include/c++/4.8.0/backward
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/include
- /usr/local/include
- /usr/lib/gcc/x86_64-unknown-linux-gnu/4.8.0/include-fixed
- /usr/include
-"
-               )))
-
-;; -----------------------------------------------
-;; JAVA MODE
-;; -----------------------------------------------
-
-;; Start eclim.
-(require 'eclim)
-(require 'eclimd)
-(global-eclim-mode)
-
-;; Setup Java eclim auto-complete.
-(require 'ac-emacs-eclim-source)
-(ac-emacs-eclim-config)
-
-;; -----------------------------------------------
-;; PYTHON MODE
-;; -----------------------------------------------
-
-;; Set Python-mode to use Jedi.
-(add-hook 'python-mode-hook 'jedi:setup)
-
-;; -----------------------------------------------
-;; LATEX MODE
-;; -----------------------------------------------
-
-;; Set auctex to PDF.
-(setq TeX-PDF-mode t)
-
-;; Load flyspell for text correction.
-(add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
-
-;; -----------------------------------------------
-;; WHITESPACE MODE
-;; -----------------------------------------------
-
-;; Highlight trailing characters
-(setq whitespace-line-column 80
-      whitespace-style '(face trailing lines-tail))
-
-;; Set invalid whitespace colors
-(set-face-attribute 'whitespace-line nil
-                    :background "red"
-                    :foreground "yellow"
-                    :weight 'bold)
-
-(set-face-attribute 'whitespace-tab nil
-                    :background "red"
-                    :foreground "yellow"
-                    :weight 'bold)
-
-;; Enable whitespace mode on everything
-(global-whitespace-mode t)
-
-;; Remove trailing whitespaces on save
-(add-hook 'write-file-hooks 'delete-trailing-whitespace)
+;; Load theme.
+(load-theme 'solarized-dark t)
--- zshrc
+++ zshrc
@@ -1,44 +1,10 @@
-# Lines configured by zsh-newuser-install
-HISTFILE=~/.histfile
-HISTSIZE=1000
-SAVEHIST=1000
-unsetopt beep
-bindkey -e
-# End of lines configured by zsh-newuser-install
-
-# The following lines were added by compinstall
-zstyle :compinstall filename '$HOME/.zshrc'
-
-autoload -Uz compinit
-compinit
-# End of lines added by compinstall
-
 # Set prompt.
-autoload -U colors && colors
-PROMPT="[%{$fg[magenta]%}%n%{$reset_color%}:%{$fg[blue]%}%~%{$reset_color%}] "
+PROMPT='%F{green}%n@%m%f:%F{cyan}%~%f%# '
 
-# Load dir colors.
-eval `dircolors $HOME/.dir_colors`
-
-# Set ls to colour output.
-alias ls="/bin/ls --color"
-
-# Set ll to detailed list.
-alias ll="ls -l"
+# Set ls output to colourized.
+alias ls='ls -G'
 
 # Set cp, mv and rm to prompt on overwrite.
 alias cp="cp -i"
 alias mv="mv -i"
 alias rm="rm -I"
-
-# Set emacs aliases.
-alias e="emacsclient -t"
-alias ec="emacsclient -c"
-alias vim="emacsclient -t"
-alias vi="emacsclient -t"
-
-# Set feh alias.
-alias feh="feh -Z --force-aliasing"
-
-# Add paths to PATH.
-PATH="$HOME/bin:/usr/local/MATLAB/R2011a/bin:$PATH"
